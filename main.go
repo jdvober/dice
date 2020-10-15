7078d696e1f1ba423f6ed15a1695e95e8e45411d
jdv <jdv@bunsentop.Home>
2020-10-15 12:24:45 -0400

    Finished version 1.0

--- main.go
+++ main.go
@@ -9,33 +9,38 @@ import (
 	"github.com/inancgumus/screen"
 )
 
+var allHeld bool = false
+var run bool = true
+var reinitialize bool = true
+var diceSlice diceStates
+var dice diceStates
+var changeDiceValues bool = true
+var numDiceVal int32
+var numSidesVal int32
+
 type dieState struct {
 	id     int32
 	sides  int32
 	value  int32
 	locked bool
+	held   bool
 }
 
 type diceStates []*dieState
 
 func (d *diceStates) RollUnlocked() {
-	// while allLocked == false do this.  Check at end of loop to see if all are locked.
-	allLocked := false
-	for allLocked == false {
-		fmt.Println("Rolling unlocked dice...\n")
-
-		for _, die := range *d {
-			s1 := rand.NewSource(time.Now().UnixNano())
-			r1 := rand.New(s1)
-			if die.locked == false {
-				die.value = r1.Int31n(die.sides) + 1
-			}
+	for _, die := range *d {
+		s1 := rand.NewSource(time.Now().UnixNano())
+		r1 := rand.New(s1)
+		if die.held == false && die.locked == false {
+			die.value = r1.Int31n(die.sides) + 1
 		}
-		d.ShowDice()
 
 	}
+	d.ShowDice()
 }
 
+// This might be redundant, but leaving until the end just in case.
 func (d *diceStates) RollAll() {
 	fmt.Println("Rolling all dice...\n")
 
@@ -50,31 +55,47 @@ func (d *diceStates) RollAll() {
 	}
 }
 
-func SetInitConditions() diceStates {
-	fmt.Println("Setting initial die states...")
+func SetConditions(changeDiceValues bool) diceStates {
+	// If the dice are not being reset, it is the first run through, and we need to create some initial variables.
+	// OR the user has chosen to change some of these variables
+	if changeDiceValues == true {
 
-	// Get number of dice from user
-	fmt.Println("How many dice would you like to roll? >> 7")
-	var numDiceVal int32 = 7 // numDicePointer := fmt.Scanln(&numDiceVal)
-	numDicePointer := &numDiceVal
+		numDicePointer := chooseNumDice()
 
-	// Assume 6 sided die for now
-	var numSidesVal int32 = 6
-	// numSidesPointer := &numSidesVal
+		// Get number of sides
+		screen.Clear()
+		fmt.Println("How many sides do these dice have?")
+		fmt.Scanln(&numSidesVal)
+		/* numSidesPointer := &numSidesVal */
 
-	// Make a container for all of the dice states (as pointers)
-	var diceSlice diceStates = make([]*dieState, *numDicePointer)
+		// Make a container for all of the dice states (as pointers)
+		diceSlice = make([]*dieState, *numDicePointer)
+
+	}
 
 	// Set each dice state's initial values, and assign to the slice as a pointer
 	for i, _ := range diceSlice {
-		var initialValues dieState = dieState{rune(i), numSidesVal, 0, false}
+		var initialValues dieState = dieState{rune(i), numSidesVal, 0, false, false}
 		diceSlice[i] = &initialValues
 	}
 	return diceSlice
 }
 
-func (d *diceStates) Lock() {
+func chooseNumDice() *int32 {
+	// Get number of dice from user
+	screen.Clear()
+	fmt.Println("\n\n\nHow many dice would you like to roll? (Max 10)")
+	fmt.Scanln(&numDiceVal)
+	for numDiceVal >= 11 || numDiceVal <= 0 {
+		screen.Clear()
+		fmt.Println("\n\n\nHow many dice would you like to roll? (Max 10)")
+		fmt.Scanln(&numDiceVal)
+	}
+	/* numDicePointer := &numDiceVal */
+	return &numDiceVal
+}
 
+func (d *diceStates) ChooseLocked() {
 	// Set up keyboard handling
 	if err := keyboard.Open(); err != nil {
 		panic(err)
@@ -98,135 +119,202 @@ func (d *diceStates) Lock() {
 		// Handle Key Presses
 
 		if key == keyboard.KeyEsc {
+			allHeld = true
 			break
 		}
-		switch char {
-		case 49: // Key "1"
-			if (*d)[0].locked == true {
-				(*d)[0].locked = false
-				d.ShowDice()
-			} else {
-				(*d)[0].locked = true
-				d.ShowDice()
+		if key == keyboard.KeySpace {
+			for _, die := range *d {
+				die.held = die.locked
 			}
-		case 50: // Key "2"
-			if (*d)[1].locked == true {
-				(*d)[1].locked = false
-				d.ShowDice()
-			} else {
-				(*d)[1].locked = true
-				d.ShowDice()
+			break
+		}
+		if char-48 <= rune(len((*d))) {
+			switch char {
+			case 49: // Key "1"
+				if (*d)[0].held == false {
+					if (*d)[0].locked == true {
+						(*d)[0].locked = false
+						d.ShowDice()
+					} else {
+						(*d)[0].locked = true
+						d.ShowDice()
+					}
+				}
+			case 50: // Key "2"
+				if (*d)[1].held == false {
+					if (*d)[1].locked == true {
+						(*d)[1].locked = false
+						d.ShowDice()
+					} else {
+						(*d)[1].locked = true
+						d.ShowDice()
+					}
+				}
+			case 51: // Key "3"
+				if (*d)[2].held == false {
+					if (*d)[2].locked == true {
+						(*d)[2].locked = false
+						d.ShowDice()
+					} else {
+						(*d)[2].locked = true
+						d.ShowDice()
+					}
+				}
+			case 52: // Key "4"
+				if (*d)[3].held == false {
+					if (*d)[3].locked == true {
+						(*d)[3].locked = false
+						d.ShowDice()
+					} else {
+						(*d)[3].locked = true
+						d.ShowDice()
+					}
+				}
+			case 53: // Key "5"
+				if (*d)[4].held == false {
+					if (*d)[4].locked == true {
+						(*d)[4].locked = false
+						d.ShowDice()
+					} else {
+						(*d)[4].locked = true
+						d.ShowDice()
+					}
+				}
+			case 54: // Key "6"
+				if (*d)[5].held == false {
+					if (*d)[5].locked == true {
+						(*d)[5].locked = false
+						d.ShowDice()
+					} else {
+						(*d)[5].locked = true
+						d.ShowDice()
+					}
+				}
+			case 55: // Key "7"
+				if (*d)[6].held == false {
+					if (*d)[6].locked == true {
+						(*d)[6].locked = false
+						d.ShowDice()
+					} else {
+						(*d)[6].locked = true
+						d.ShowDice()
+					}
+				}
+			case 56: // Key "8"
+				if (*d)[7].held == false {
+					if (*d)[7].locked == true {
+						(*d)[7].locked = false
+						d.ShowDice()
+					} else {
+						(*d)[7].locked = true
+						d.ShowDice()
+					}
+				}
+			case 57: // Key "9"
+				if (*d)[8].held == false {
+					if (*d)[8].locked == true {
+						(*d)[8].locked = false
+						d.ShowDice()
+					} else {
+						(*d)[8].locked = true
+						d.ShowDice()
+					}
+				}
+			case 48: // Key "0"
+				if char-40 <= rune(len((*d))) {
+					if (*d)[9].held == false {
+						if (*d)[9].locked == true {
+							(*d)[9].locked = false
+							d.ShowDice()
+						} else {
+							(*d)[9].locked = true
+							d.ShowDice()
+						}
+					}
+				}
 			}
-		case 51: // Key "3"
-			if (*d)[2].locked == true {
-				(*d)[2].locked = false
-				d.ShowDice()
-			} else {
-				(*d)[2].locked = true
-				d.ShowDice()
-			}
-		case 52: // Key "4"
-			if (*d)[3].locked == true {
-				(*d)[3].locked = false
-				d.ShowDice()
-			} else {
-				(*d)[3].locked = true
-				d.ShowDice()
-			}
-		case 53: // Key "5"
-			if (*d)[4].locked == true {
-				(*d)[4].locked = false
-				d.ShowDice()
-			} else {
-				(*d)[4].locked = true
-				d.ShowDice()
-			}
-		case 54: // Key "6"
-			if (*d)[5].locked == true {
-				(*d)[5].locked = false
-				d.ShowDice()
-			} else {
-				(*d)[5].locked = true
-				d.ShowDice()
-			}
-		case 55: // Key "7"
-			if (*d)[6].locked == true {
-				(*d)[6].locked = false
-				d.ShowDice()
-			} else {
-				(*d)[6].locked = true
-				d.ShowDice()
-			}
-		case 56: // Key "8"
-			if (*d)[7].locked == true {
-				(*d)[7].locked = false
-				d.ShowDice()
-			} else {
-				(*d)[7].locked = true
-				d.ShowDice()
-			}
-		case 57: // Key "9"
-			if (*d)[8].locked == true {
-				(*d)[8].locked = false
-				d.ShowDice()
-			} else {
-				(*d)[8].locked = true
-				d.ShowDice()
-			}
-		case 48: // Key "0"
-			if (*d)[9].locked == true {
-				(*d)[9].locked = false
-				d.ShowDice()
-			} else {
-				(*d)[9].locked = true
-				d.ShowDice()
-			}
-		default:
-			// Do nothing
 		}
 	}
-
 }
 
 func (d *diceStates) ShowDice() {
 	screen.Clear()
-	fmt.Println("\nPress the number of the die to toggle locked / unlocked state.  Press Esc to lock in.")
+	fmt.Println("\nPress the number of the die to toggle locked / unlocked state.\nPress Space to hold the dice that you have locked in.\nPress Esc to finish")
 	fmt.Println("\n{Dice Number, Value, Lock State}  ")
 	/* fmt.Printf("\n{Dice Number, Value, Lock State}  You pressed: rune %q, key %X\r\n", char, key) */
 	for _, die := range *d {
-		if die.locked == false {
-			fmt.Printf("Die %d: %d\tUnlocked\n", die.id, die.value)
-		} else {
-			fmt.Printf("Die %d: %d\tLocked\n", die.id, die.value)
+		switch die.held {
+		case true:
+			fmt.Printf("Die %d: %d\tHeld\n", die.id+1, die.value)
+		default:
+			switch die.locked {
+			case true:
+				fmt.Printf("Die %d: %d\tKeep\n", die.id+1, die.value)
+			case false:
+				fmt.Printf("Die %d: %d\tRe-roll\n", die.id+1, die.value)
+			}
 		}
 	}
 }
 
-func (d *diceStates) CheckAllLocked() bool {
+func (d *diceStates) CheckAllHeld() bool {
+	if (*d)[0].held == false {
+		return false
+	}
 	for i := 1; i < len((*d)); i++ {
 		// If the index you are checking is not the same as index zero, they can not all be the same.  If you get to the end, you haven't returned yet, so they all must be the same, and you should return true.
-		if (*d)[i] != (*d)[0] {
+		if (*d)[i].held != (*d)[0].held {
 			return false
 		}
 	}
+	d.ShowDice()
 	return true
-
 }
 
 func main() {
+	for run == true {
+		var menuChoice int
 
-	// Initalize the dice
-	dice := SetInitConditions()
+		if reinitialize == true {
+			// Initalize the dice
+			dice = SetConditions(true)
+			allHeld = false
+		} else {
+			dice = SetConditions(false)
+		}
 
-	// Roll dice
-	dice.RollUnlocked()
+		// While allHeld is false, continue to re-roll unlocked dice
+		for allHeld == false {
+			dice.RollUnlocked()
 
-	// Ask user which they would like to keep / are they done rolling
-	dice.Lock()
+			// Ask user which they would like to keep / are they done rolling
+			dice.ChooseLocked()
 
-	// Repeat until no dice or done rolling, then reset dice.
-	// Check to see if all dice are locked in.  If they are, the turn is over, and we should flag this somehow.  If not, main should re-roll unlocked dice.
-	d.CheckAllLocked() // Returns true if all dice are the same.  This line should only evaluate if index 0 is locked, because the default is unlocked (false)
-	// Add in error checking so that you can not unlock dice that were previously locked
+			/* Check to see if all dice are held.  If they are, the turn is over, and we should flag this somehow.  If not, main should re-roll unlocked dice. */
+			allHeld = dice.CheckAllHeld()
 
+		}
+		// Ask if you would like to reset dice and take a new turn, or quit.
+
+		fmt.Printf("\nAll dice are held.  Your turn is over.  Would you like to roll a new set of dice, or would you like to end the program?\nEnter:\n( 1 ) to play again with the same number of dice\n( 2 ) to choose a new number of dice\n(Any other entry) Quit.\n")
+		fmt.Scanln(&menuChoice)
+		switch menuChoice {
+		case 1:
+			reinitialize = false
+			allHeld = false
+			run = true
+			break
+			// Do nothing, run allHeld loop again
+		case 2:
+			reinitialize = true
+			allHeld = false
+			run = true
+			break
+		default:
+			screen.Clear()
+			fmt.Println("\nThanks for playing!")
+			run = false
+			break
+		}
+	}
 }
